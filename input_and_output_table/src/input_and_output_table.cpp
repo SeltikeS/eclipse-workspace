6a5cf6f386bc8dc6040cfcc09cb05e00259e2d19
SeltikeS <pschenka@gmail.com>
2021-11-30 23:18:36 +0300

    correct last exercise

--- input_and_output_table/src/input_and_output_table.cpp
+++ input_and_output_table/src/input_and_output_table.cpp
@@ -19,25 +19,13 @@ int main() {
 
 	int n, m;
 	input >> n >> m;
-	vector<vector<int>> table;
-
-	for(int i = 0; i < n; ++i) {
-		int number;
-		vector<int> raw;
-		for(int j = 0; j < m-1; ++j) {
-			int number;
-			input >> number;
-			raw.push_back(number);
-			input.ignore(1);
-		}
-		input >> number;
-		raw.push_back(number);
-		table.push_back(raw);
-	}
 
 	for(int i = 0; i < n; ++i) {
 		for(int j = 0; j < m; ++j) {
-			cout << setw(10) << table[i][j];
+			int number;
+			input >> number;
+			cout << setw(10) << number;
+			input.ignore(1);
 			if(j < m-1) {
 				cout << ' ';
 			}
@@ -46,6 +34,7 @@ int main() {
 			cout << endl;
 		}
 	}
+
 
 	return 0;
 }
